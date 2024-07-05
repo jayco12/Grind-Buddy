b087b69f000de85acea92787cb7404395c19f10a
apanga01 <pallyssam@gmail.com>
2024-07-05 18:51:55 -0400

    updated about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+        <div className="about-sections">
+            <div className="about-intro">
+                <h1>HEADING</h1>
+                <p>Lorem ipsum dolor sit amet, <b>consectetur</b> adipiscing elit, consectetur adipiscing elit, sed do eiusmod tempor <b>incididunt</b> ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="about-left">
+                <div className="about-details">
+                    <li></li>
+                </div>
+            </div>
+            <div className="about-right">
+                
+            </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>100+</h1>
+                <p>USERS CONNECTED</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>50+</h1>
+                <p>SCHOOLS PARTICIPATED</p>
+            </div>
+            <hr /><div className="about-achievement">
+                <h1>100+</h1>
+                <p>USERS CONNECTED</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
