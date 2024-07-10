9ee175965fc5122cec7441e5b61a4544833d34f1
apanga01 <pallyssam@gmail.com>
2024-07-10 00:03:18 -0400

    update about team section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,8 +11,8 @@ const AboutPage = () => {
           <h1>Blurb about team's goal/interest in making this.</h1>
           <p>(Add about our team) - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <div>
-
+        <div className="about-us-team">
+          <h2>Meet the Team</h2>
         </div>
         <div className="about-us-container">
         {Team_Data.map((team,index)=>{
