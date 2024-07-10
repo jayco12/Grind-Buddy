09c951414cdfb53e235db4e195719cb698ab892a
apanga01 <pallyssam@gmail.com>
2024-07-10 00:17:09 -0400

    update layout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,20 @@
 import './page.css'
 import arrow_forward from '../../../public/assets/arrow_forward.png'
 import Team_Data from '../../../public/assets/team_data.jsx'
-
+import Image from 'next/image'
+import purple_hue from '../../../public/assets/purple_hue.png'
+import blue_hue from '../../../public/assets/blue_hue.png'
 
 const AboutPage = () => {
 
   return (
     <div className="about-us">
+      <div className="design-element1">
+            <Image src={purple_hue} alt="Design Element" layout="fill" objectFit="contain" />
+        </div>
+        <div className="design-element2">
+            <Image src={blue_hue} alt="Design Element" layout="fill" objectFit="contain" />
+        </div>
         <div className="about-us-intro">
           <h1>Blurb about team's goal/interest in making this.</h1>
           <p>(Add about our team) - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
